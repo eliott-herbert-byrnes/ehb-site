f13fa8bab3ee0500e485ccb2a248a581980f9338
eliott-herbert-byrnes <eliott.c.h.byrnes@googlemail.com>
2025-12-01 09:26:03 +0000

    fix: weblinks

--- features/web-link/web-link.tsx
+++ features/web-link/web-link.tsx
@@ -1,15 +1,31 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { ArrowUpRightIcon } from "lucide-react";
 
-export function WebLink({ href, title, className }: { href: string; title: string; className?: string }) {
+export function WebLink({
+  href,
+  title,
+  className,
+}: {
+  href: string;
+  title: string;
+  className?: string;
+}) {
+  const handleClick = () => {
+    window.open(href, "_blank");
+  };
   return (
-    <Card className={className}>
+    <Card className={className} onClick={handleClick}>
       <CardContent>
         <div className="flex items-center gap-2 justify-between">
-          <a href={href} target="_blank" rel="noopener noreferrer" className="font-medium">
+          <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-medium"
+          >
             {title}
+            <ArrowUpRightIcon className="w-4 h-4 text-muted-foreground" />
           </a>
-          <ArrowUpRightIcon className="w-4 h-4 text-muted-foreground" />
         </div>
       </CardContent>
     </Card>
